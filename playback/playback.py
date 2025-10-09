2af2b1b7339b4923d0823a3508c4c17b94ee52e1
Madhav Khanijo <raiden.curious15@gmail.com>
2025-10-09 23:48:37 +0530

    playback.py

---
+++ playback/playback.py
@@ -0,0 +1,6 @@
+#Playback speed- Python program that prompts the user for input and then outputs that same input, replacing each space with ... (i.e., three periods).
+
+#Whatver the user inputs has been given the name text
+text=input("Whatever you enter would be returned with three periods-")
+output=text.replace(" ","...")
+print(output)
