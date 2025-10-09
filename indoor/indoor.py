f507e65cd0441706f4101b785fcfa4834f0ce866
Madhav Khanijo <raiden.curious15@gmail.com>
2025-10-09 23:47:04 +0530

    indoor.py

---
+++ indoor/indoor.py
@@ -0,0 +1,6 @@
+#Indoor Voice- Python program that prompts the user for input and then outputs that same input in lowercase(Punctuation and white spaces remain unaffected).
+
+#Whatever the user provides as input has been given the name text
+text=input("Write whatever you want to get printed in lowercase:")
+text=text.lower()
+print("Here is your text printed in lowercase-" , text)
