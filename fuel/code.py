78768aa8d7d0f01c340d44e00697a388b0f5ff22
Madhav Khanijo <raiden.curious15@gmail.com>
2026-03-31 23:43:15 +0530

    Create code.py

---
+++ fuel/code.py
@@ -0,0 +1,25 @@
+while True:
+    try:
+        frac=input("Enter fraction (x/y): ")
+        x,y = frac.split("/")
+        x=int(x)
+        y=int(y)
+
+        if y==0:
+            raise ZeroDivisionError
+
+        if x>y:
+            raise ValueError
+
+    except (ValueError, ZeroDivisionError):
+        continue
+    else:
+        break
+z= round((x/y)*100)
+
+if z<=1 :
+    print("Your fuel percentage is: E")
+elif z>=99:
+    print("Your fuel percentage is: F")
+else:
+    print(f"{z}%")
